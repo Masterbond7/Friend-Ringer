dfed8042565b70822efe0629afd660fe82ea0197
Masterbond7 <quintinslough@gmail.com>
2021-10-10 14:04:16 +1300

    Added more comments

--- Server/main.py
+++ Server/main.py
@@ -22,22 +22,30 @@ server.listen(1)
 # Main loop
 while True:
     try:
+        # Accept connection
         clientdata = {}
         client, address = server.accept()
 
+        # State that a device has connected
         print("Connected to: {ip}:{port}".format(ip=address[0], port=address[1]))
         
+        # Get the name and ID data and store/update it in the data variable
         clientdata["id"], clientdata["name"] = client.recv(1024).decode().replace("\r\n", "").split("|")
         if not any(userdata["id"] == clientdata["id"] for userdata in data):
             data.append(clientdata)
         else:
             data[int(clientdata["id"])] = clientdata
 
+        # Display the newly acquired information as well as all the data stored
         print("Connection ID: {id}, Name: {name}".format(id=clientdata["id"], name=clientdata["name"]))
         print(data, end="\n\n")
+
+        # Close the connection
         client.close()
+    
+    # Stop errors from crashing the program (mostly to stop me being stupid while manually testing)
     except Exception as e:
-        print("An error has occured,"+str(e))
+        print("An error has occurred,"+str(e))
 
 # Close socket
-server.close()
+server.close() # NOTE: this code is unreachable
