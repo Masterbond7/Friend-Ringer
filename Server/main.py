9322ddcacdffd1f4225b5900ca183bac5e6539aa
Masterbond7 <quintinslough@gmail.com>
2021-10-10 13:55:00 +1300

    Basic data storage and updating.

--- Server/main.py
+++ Server/main.py
@@ -2,10 +2,13 @@
 print("Importing Libraries")
 import socket
 
+# Creating data
+data = []
+
 # Starting socket
 print("Starting socket")
 server = socket.socket()
-host = "0.0.0.0"
+host = ""
 port = 6000
 
 # Binding socket
@@ -18,11 +21,23 @@ server.listen(1)
 
 # Main loop
 while True:
-    client, address = server.accept()
+    try:
+        clientdata = {}
+        client, address = server.accept()
 
-    print("Connected to: {ip}:{port}".format(ip=address[0], port=address[1]))
-    client.send("Hello World!\n".encode("ASCII"))
-    client.close()
+        print("Connected to: {ip}:{port}".format(ip=address[0], port=address[1]))
+        
+        clientdata["id"], clientdata["name"] = client.recv(1024).decode().replace("\r\n", "").split("|")
+        if not any(userdata["id"] == clientdata["id"] for userdata in data):
+            data.append(clientdata)
+        else:
+            data[int(clientdata["id"])] = clientdata
+
+        print("Connection ID: {id}, Name: {name}".format(id=clientdata["id"], name=clientdata["name"]))
+        print(data, end="\n\n")
+        client.close()
+    except Exception as e:
+        print("An error has occured,"+str(e))
 
 # Close socket
 server.close()
