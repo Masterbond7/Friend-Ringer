868e1baf6d491c2c73549538d479d8e8fe2053ba
Masterbond7 <jamesslough4@gmail.com>
2021-10-12 21:55:34 +1300

    Added jinja render template for flask to use.

--- Server/main.py
+++ Server/main.py
@@ -1,10 +1,10 @@
 # Importing libraries
 print("Importing Libraries")
-import socket                # Networking
-import os, json              # Saving and loading data
-from threading import Thread # Multithreading
-from flask import Flask      # Web server
-import waitress              # Web server
+import socket                            # Networking
+import os, json                          # Saving and loading data
+from threading import Thread             # Multithreading
+from flask import Flask, render_template # Web server
+import waitress                          # Web server
 
 from network_handler import handle_connection
 
@@ -58,12 +58,13 @@ website = Flask(__name__)
 
 @website.route('/')
 def index():
-    return "Hello World!"
+    return render_template("index.jinja")
 
 
 if __name__ == "__main__":
-    flask_thread = Thread(target=lambda: waitress.serve(website, host="0.0.0.0", port=5000))
+    #flask_thread = Thread(target=lambda: waitress.serve(website, host="0.0.0.0", port=5000))
     networking_thread = Thread(target=thread_networking)
 
-    flask_thread.start(); print("Hosted website")
+    #flask_thread.start(); print("Hosted website")
     networking_thread.start()
+    website.run(host="", port=5000, debug=True)
