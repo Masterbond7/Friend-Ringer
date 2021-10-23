27b53f2e5da886e891738abe9687c584a1a02091
Masterbond7 <jamesslough4@gmail.com>
2021-10-23 16:14:00 +1300

    Added a text file containing the planned EEPROM allocation and some code to test the EEPROM

--- Node/main/main.ino
+++ Node/main/main.ino
@@ -1,7 +1,15 @@
+#include <EEPROM.h>
+
+int old_value;
+
 void setup() {
     Serial.begin(9600);
+    EEPROM.begin(1);
+    old_value = EEPROM.read(0);
+    EEPROM.write(0, old_value+1);
+    EEPROM.commit();
 }
 
 void loop() {
-    Serial.println("Hello World!");
+    Serial.println(old_value);
 }
